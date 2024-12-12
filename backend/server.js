74be7edc3f1eca60f9dd094c9346fb29ebe14c6f
Ayberk Yavas <ayberkyavas9@gmail.com>
2024-12-12 20:27:34 +0300

    Make secure Cloudinary and Express configurations using environment variables

--- backend/server.js
+++ backend/server.js
@@ -15,14 +15,14 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
-    secure: true,
+    secure: process.env.CLOUDINARY_SECURE,
 });
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json({limit: '2mb'}));
-app.use(express.urlencoded({ extended: true }, {limit: '2mb'}));
+app.use(express.json({limit: process.env.FILE_LIMIT}));
+app.use(express.urlencoded({ extended: true }, {limit: process.env.FILE_LIMIT}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
